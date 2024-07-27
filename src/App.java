9ad88e08a6b4244621d0f4e6f40049291971fda4
EssejOliveira <jesse.os@outlook.com>
2024-07-26 23:00:11 -0300

    teste de novo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("alterei para teste!");
+        System.out.println("alterei novamente teste!");
     }
 }
